794a9b16af6f80b2f2cd4c56300e6cd681e7369c
buddywinangun <buddywinangun@gmail.com>
2022-11-21 17:28:01 +0700

    edit theme admin

--- application/views/admin/templates/welcome/404.php
+++ application/views/admin/templates/welcome/404.php
@@ -2,4 +2,8 @@
 
 <p class="lead">Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem.</p>
 
+<<<<<<< HEAD
 <?= apply_filters('anchor_missing', anchor('/', 'return home')); ?>
+=======
+<?= apply_filters('anchor_missing', admin_anchor('', 'return home')); ?>
+>>>>>>> features
