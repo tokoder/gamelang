2a45008a065dad9e8032f5cc64c1fd8724ff85c3
awanputih <awanwinangun@gmail.com>
2023-01-31 17:42:18 +0700

    edit themes default

--- application/themes/default/templates/welcome/error_404.php
+++ application/themes/default/templates/welcome/error_404.php
@@ -1,5 +1,5 @@
-<h1>Opps! Page not found.</h1>
+<h1><?php _e('Opps! Page not found.')?></h1>
 
-<p class="lead">Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem.</p>
+<p class="lead"><?php _e('Sorry, the page you\'re looking for doesn\'t exist. If you think something is broken, report a problem.')?></p>
 
 <?= apply_filters('anchor_missing', anchor('/', 'return home')); ?>
